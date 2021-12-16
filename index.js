f2ea05e3b41adb92d019a56803c8559c01276f87
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2021-12-16 17:11:51 +0100

    thread blocking fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
+import { promises as fs } from "fs";
 import parser from "xml-js";
 
 const app = express();
@@ -8,28 +8,28 @@ const app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/folders", (req, res) => {
+app.get("/folders", async (req, res) => {
   // send folder names
-  const folders = fs.readdirSync("./data");
+  const folders = await fs.readdir("./data");
   res.send(folders);
 });
 
-app.post("/read", (req, res) => {
+app.post("/read", async (req, res) => {
   // get all images and their annotations
   const folder = req.body.folder;
-  const images = fs.readdirSync(`./data/${folder}/images`);
-  let data = [];
-  images.forEach((jpg) => {
-    const b64 = fs.readFileSync(`./data/${folder}/images/${jpg}`, "base64");
+  const images = await fs.readdir(`./data/${folder}/images`);
+  let data = images.map(async (jpg) => {
+    const b64 = await fs.readFile(`./data/${folder}/images/${jpg}`, "base64");
     const xml = jpg.replace(".jpg", ".xml");
-    const annotation = fs.readFileSync(`./data/${folder}/annotations/${xml}`);
+    const annotation = await fs.readFile(`./data/${folder}/annotations/${xml}`);
     const js = parser.xml2js(annotation, { compact: true });
-    data.push({ jpg: jpg, image: b64, annotation: js.annotation });
+    return { jpg: jpg, image: b64, annotation: js.annotation };
   });
+  data = await Promise.all(data);
   res.send(data);
 });
 
-app.post("/write", (req, res) => {
+app.post("/write", async (req, res) => {
   // overwrite a single annotation file
   const folder = req.body.folder;
   const jpg = req.body.image;
@@ -37,14 +37,15 @@ app.post("/write", (req, res) => {
   const danger = req.body.danger;
   const xml = jpg.replace(".jpg", ".xml");
   const xmlPath = `./data/${folder}/annotations/${xml}`;
-  const xmlConent = fs.readFileSync(xmlPath);
-  let js = parser.xml2js(xmlConent, { compact: true });
+  const xmlContent = await fs.readFile(xmlPath);
+  let js = parser.xml2js(xmlContent, { compact: true });
   let objects = js.annotation.object;
   if (!Array.isArray(objects)) objects = [objects];
   objects[sub].danger = { _text: danger };
   const json = JSON.stringify(js);
   let xmlNew = parser.json2xml(json, { compact: true, spaces: 2 });
-  fs.writeFileSync(xmlPath, xmlNew);
+  await fs.writeFile(xmlPath, xmlNew);
+  res.send({});
 });
 
 const PORT = 8000;
